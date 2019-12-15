6ede85a4b6185550d71b5f053d2635bc3d05260e
gngonoessono <58873038+gngonoessono@users.noreply.github.com>
2019-12-15 16:34:44 -0600

    adding ngonoessono_a02b.js

---
+++ ngonoessono_a02b.js
@@ -0,0 +1,16 @@
+/*Gertrude Vanessa Ngono Essono
+ngonoessono_a02b.js
+INFO2124
+Thoendel
+12/14/2019*/
+
+let fullName="Gertrude Vanessa Ngono Essono";
+myAge=27;
+const myHairColor="black";
+const myEyeColor="brown";
+console.log("About me v.1.0" + "\n" + "--------------"+ "\n" +"--------------") ;
+console.log("My name" + ":" +"\n" + fullName);
+console.log("My age" + ":" + "\n" + myAge);
+console.log("My hair color" + ":" + "\n"  + myHairColor);
+console.log("My eye color" + ":" + "\n" + myEyeColor);
+console.log();
